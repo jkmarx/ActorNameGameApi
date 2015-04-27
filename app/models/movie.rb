f48e81d0ee2a7ae6a761dbfb2ad60d99e28f891b
J.Marx <jennyrutledge@gmail.com>
2015-04-26 22:54:13 -0400

    Error found when the profile is missing on TMDB api. Fixed with a cast profile validator

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,16 +2,30 @@ class Movie
   include ActiveModel::Model
 
   def self.getPersonsData(arrId)
-    arrId.map{ |personId| JSON.parse(MovieDetail::requestPerson(personId).body)["person_results"][0] }
+    JSON.parse(MovieDetail::requestPerson(arrId).body)["person_results"][0]
   end
 
   def self.parseNameImage(data)
     data.map{ |person| {name: person["name"], image: "https://image.tmdb.org/t/p/w185" + person["profile_path"]} }
   end
 
+  def self.addValidCast(castIds)
+    validCast = []
+    ind = 0
+    while validCast.length < 6 && ind < castIds.length
+      tempActor = getPersonsData(castIds[ind])
+      if tempActor && tempActor["name"] && tempActor["profile_path"]
+        validCast << tempActor
+      end
+      ind = ind + 1
+    end
+    validCast
+  end
+
   def self.getFiveCastImage(movieId)
     castIds = MovieDetail::getMovieCastIds(movieId)
-    personsDetails = getPersonsData(castIds[0..4])
+    personsDetails = addValidCast(castIds)
     parseNameImage(personsDetails)
   end
+
 end
